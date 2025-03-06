5908343c3821b01bcd0f608796db26db8e27c28c
ji-podhead <you@example.com>
2025-03-06 13:34:56 +0100

    changed workflow

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 cd website/app
 #bun run build
 cd ../../
-git add build.sh
-git stash
-git checkout -d build
-git switch build
-git stash
-git add -f ./website/app/build
+
+git add ./website/app/build/ --force
+git checkout build
 git commit -m "Deploy to build"
 git push origin build
 #sed -i 's/"homepage": "https:\/\/pam-ji.github.io\/.github\/"/"homepage": "https:\/\/pamji.space"/g' package.json
